ff8a4ec7bc88b1268e292a940920c136ed6b36d8
Urhengulas <joheihe@web.de>
2019-06-22 14:36:49 +0200

    test_request_context
    Used to test HTTP requests locally

--- app.py
+++ app.py
@@ -23,3 +23,10 @@ def hello_name(name):
 @app.route('/path/<path:subpath>/')
 def show_subpath(subpath):
     return f'Subpath: {subpath}'
+
+
+print("\n### OUTPUT ###")
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('hello_name', name='anna'))
+print("##############\n")
