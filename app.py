df19e1cb9ee5ca4d3af97aebb140933803742057
Urhengulas <joheihe@web.de>
2019-06-22 16:15:23 +0200

    render_templates
    jinja2-templates are used to render html

--- app.py
+++ app.py
@@ -1,43 +1,26 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    hello = "<a href=/hello>Say Hello<a/>"
-    path = "<a href=/path/i/am/a/path>Dig deep<a/>"
-    routes = [hello, path]
-    return "<br>".join(routes)
+    return render_template('index.html')
 
 
 @app.route('/hello/')
-def hello_flask():
-    return f'Hello, Flask!'
-
-
 @app.route('/hello/<string:name>/')
-def hello_name(name):
-    return f'Hello, {name.title()}!'
+def hello_name(name=None):
+    try:
+        name = name.title()
+    except:
+        pass
+
+    return render_template('hello.html', name=name)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return do_login()
+        return render_template("do_login.html")
     else:
-        return show_login_form()
-
-
-def do_login():
-    return "<h3>logging you in<h3/>"
-
-
-def show_login_form():
-    return """
-    <form>
-        Username:<br>
-        <input type="text" name="username"><br>
-        Password:<br>
-        <input type="text" name="password">
-    </form>
-    """
+        return render_template("login_form.html")
