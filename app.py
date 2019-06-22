b5b0c0c6bdec25a2aec86070742de337feb70d38
Urhengulas <joheihe@web.de>
2019-06-22 14:27:27 +0200

    Flask variables

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Flask!'
+@app.route('/hello/')
+def hello_flask():
+    return f'Hello, Flask!'
+
+
+@app.route('/hello/<string:name>/')
+def hello_name(name):
+    return f'Hello, {name.title()}!'
